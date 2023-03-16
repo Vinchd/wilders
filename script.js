b4b4ae0c2df55036b0d21438e3c3c8dc2cf6472e
Vinch <vince.daviaud@gmail.com>
2023-03-16 11:35:32 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+
+
+
+console.log(chalk.blue("Sébastien Birolleau"))
+console.log(chalk.green("Enzo Castagnos"))
+console.log(chalk.yellow("Hafsa Ait Ouarraou"))
+console.log(chalk.grey("Thibaut Resse"))
